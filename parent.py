ed44f076227263a4dcb719725ffe1c53859bd93b
Ezharjan <ezharjan@outlook.com>
2022-10-26 10:33:28 +0800

    simple test

---
+++ parent.py
@@ -0,0 +1,9 @@
+class ParentClass():
+    def anAbstractMethod(self):
+        print('abstract')
+
+    def parentMethod(self):
+        print('I am a child method.')
+
+    def otherMethod():
+        print("other methods")
