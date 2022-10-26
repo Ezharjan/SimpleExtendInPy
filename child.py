11b7f55d1aeed4434b0a55dc9e75389adbd8948d
Ezharjan <ezharjan@outlook.com>
2022-10-26 10:29:31 +0800

    simple test

---
+++ child.py
@@ -0,0 +1,23 @@
+class ChildClass: 
+    def __init__(self):
+        from importlib import import_module
+        instance=import_module('parent')
+        self.myClass=instance.ParentClass()
+    
+    def methodX(self):
+        self.myClass.parentMethod()
+
+    def anAbstractMethod(myClass):
+        print("re-implemented by child!")
+    
+    def methodY(self):
+        self.myClass.anAbstractMethod()
+        self.myClass.anAbstractMethod = self.anAbstractMethod 
+        self.myClass.anAbstractMethod() 
+        print('I am method Y in child class!')
+    
+
+
+instance = ChildClass()
+instance.methodX()
+instance.methodY()
